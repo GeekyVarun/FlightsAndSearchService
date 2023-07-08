eea2b0306067b5f7d125b6af546916787e26b5b3
Varun <vgupta168@gmail.com>
2023-07-08 22:35:51 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,7 +4,9 @@ class CityRepository {
 
     async createCity({ name }) { 
         try {
-            const city = await City.create({name});
+            const city = await City.create({
+                name
+            });
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
@@ -20,6 +22,29 @@ class CityRepository {
                 }
             });
             return true;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) { 
+        try {        
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });    
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw {error};
